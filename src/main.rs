3030bb93fc10d6ef24b3d79807d62b256a4b122a
Eric Stolten <stoltene2@gmail.com>
2021-10-20 09:25:57 -0400

    Implement base station with borrowing

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,19 @@
 #[derive(Debug)]
 struct CubeSat {
     id: u64,
+    mailbox: Mailbox,
 }
 
 impl CubeSat {
     fn new(id: u64) -> CubeSat {
-        CubeSat { id }
+        CubeSat {
+            id,
+            mailbox: Mailbox { messages: vec![] },
+        }
+    }
+
+    fn receive(&mut self) -> Option<Message> {
+        self.mailbox.messages.pop()
     }
 }
 
@@ -15,18 +23,37 @@ enum StatusMessage {
     Ok,
 }
 
+#[derive(Debug)]
+struct Mailbox {
+    messages: Vec<Message>,
+}
+
+type Message = String;
+
+struct GroundStation;
+
+impl GroundStation {
+    fn send(&self, to: &mut CubeSat, msg: Message) {
+        to.mailbox.messages.push(msg);
+    }
+}
+
 fn check_status(cube_sat: CubeSat) -> CubeSat {
     println!("{:?}", StatusMessage::Ok);
     cube_sat
 }
 
 fn main() {
-    let sat_a = CubeSat::new(0);
-    let sat_b = CubeSat::new(1);
-    let sat_c = CubeSat::new(2);
+    let gs = GroundStation {};
 
-    let sat_a = check_status(sat_a);
-    let sat_b = check_status(sat_b);
-    let sat_c = check_status(sat_c);
-    println!("a: {:?}, b: {:?}, c: {:?}", sat_a, sat_b, sat_c);
+    let mut sat_a = CubeSat::new(0);
+
+    println!("t0: {:?}", sat_a);
+    gs.send(&mut sat_a, Message::from("Hello from Earth"));
+
+    println!("t1: {:?}", sat_a);
+    let msg = sat_a.receive();
+
+    println!("t2: {:?}", sat_a);
+    println!("msg: {:?}", msg);
 }
