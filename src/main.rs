2317901b66f4977f14051e0ff1a44f1fa932f219
Eric Stolten <stoltene2@gmail.com>
2021-10-29 09:47:16 -0400

    Represent failure and handle message ownership between data structures

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ impl CubeSat {
         }
     }
 
-    fn receive(&mut self) -> Option<Message> {
-        self.mailbox.messages.pop()
+    fn receive(&self, mailbox: &mut Mailbox) -> Option<Message> {
+        mailbox.deliver(self)
     }
 }
 
@@ -28,13 +28,39 @@ struct Mailbox {
     messages: Vec<Message>,
 }
 
-type Message = String;
+impl Mailbox {
+    /// Adds messages to the outgoing msg queue for sending to `CubeSat`s
+    fn post(&mut self, msg: Message) -> () {
+        self.messages.push(msg)
+    }
+
+    /// Deliver messages to the `CubeSat`s
+    fn deliver(&mut self, cube_sat: &CubeSat) -> Option<Message> {
+        for i in 0..self.messages.len() {
+            if cube_sat.id == self.messages[i].to {
+                return Some(self.messages.remove(i));
+            }
+        }
+
+        None
+    }
+}
+
+#[derive(Debug)]
+struct Message {
+    to: u64,
+    conent: String,
+}
 
 struct GroundStation;
 
 impl GroundStation {
-    fn send(&self, to: &mut CubeSat, msg: Message) {
-        to.mailbox.messages.push(msg);
+    fn connect(&self, sat_id: u64) -> CubeSat {
+        CubeSat::new(sat_id)
+    }
+
+    fn send(&self, mailbox: &mut Mailbox, msg: Message) {
+        mailbox.post(msg)
     }
 }
 
@@ -43,17 +69,29 @@ fn check_status(cube_sat: CubeSat) -> CubeSat {
     cube_sat
 }
 
+fn fetch_sat_ids() -> Vec<u64> {
+    vec![1, 2, 3]
+}
+
 fn main() {
     let gs = GroundStation {};
 
-    let mut sat_a = CubeSat::new(0);
+    let mailbox = &mut Mailbox {
+        messages: Vec::new(),
+    };
 
-    println!("t0: {:?}", sat_a);
-    gs.send(&mut sat_a, Message::from("Hello from Earth"));
+    let sats: Vec<CubeSat> = fetch_sat_ids()
+        .iter()
+        .map(|sat_id| -> CubeSat { gs.connect(*sat_id) })
+        .collect();
 
-    println!("t1: {:?}", sat_a);
-    let msg = sat_a.receive();
-
-    println!("t2: {:?}", sat_a);
-    println!("msg: {:?}", msg);
+    for sat in sats {
+        let msg = Message {
+            to: sat.id,
+            conent: String::from("Hello sat"),
+        };
+        gs.send(mailbox, msg);
+        let msg = sat.receive(mailbox).unwrap();
+        println!("msg: {:?}", msg);
+    }
 }
